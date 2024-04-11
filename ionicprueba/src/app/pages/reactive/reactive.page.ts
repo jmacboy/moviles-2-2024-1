59f422682e6f025d2c52a9233a47d351ddd4d9e2
Jose Alvarez <j.m.alvarez.camacho@gmail.com>
2024-04-11 08:41:38 -0400

    agregados forms reactivos

--- ionicprueba/src/app/pages/reactive/reactive.page.ts
+++ ionicprueba/src/app/pages/reactive/reactive.page.ts
@@ -8,10 +8,21 @@ import { FormBuilder, FormControl, Validators } from '@angular/forms';
 })
 export class ReactivePage {
   personaForm = this.formBuilder.group({
-    nombres: ['', Validators.required]
+    nombres: ['', Validators.required],
+    apellidos: ['', Validators.required],
+    edad: ['', [
+      Validators.required,
+      Validators.min(1),
+      Validators.max(99)
+    ]
+    ],
+    ciudad: ['', Validators.required],
+    fechaNacimiento: ['', Validators.required],
   });
 
   constructor(private formBuilder: FormBuilder) { }
 
-
+  saveForm() {
+    console.log(this.personaForm.value);
+  }
 }
